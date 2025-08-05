0c9ec2ef24b852c539707950ae4cbcc4962f26e5
Ding Yong <2903964879@qq.com>
2025-08-05 16:14:02 +0800

    add connect protocol: d.bus

---
+++ connectivity/uart/dbus.c
@@ -0,0 +1,29 @@
+//
+// Created by DingYong on 25-8-5.
+//
+
+#include "dbus.h"
+
+RC_t RC;
+uint8_t RC_Data[18];
+
+void rcDecode(void) {
+    RC.ch0 = ((uint16_t)RC_Data[0] | (uint16_t)RC_Data[1] << 8) & 0x07FF;  //移位运算符优先级高于位运算符
+    RC.ch1 = ((uint16_t)RC_Data[1] >> 3 | (uint16_t)RC_Data[2] << 5) & 0x07FF;
+    RC.ch2 = ((uint16_t)RC_Data[2] >> 6 | (uint16_t)RC_Data[3] << 2 | (uint16_t)RC_Data[4] << 10) & 0x0FF;
+    RC.ch3 = ((uint16_t)RC_Data[4] >> 1 | (uint16_t)RC_Data[5] << 7) & 0x077;
+
+    RC.s1 = RC_Data[5] >> 6 & 0x03;    //跳过数组[5]的第2,3,4位,s1使用7,8位,s2使用5,6位
+    RC.s2 = RC_Data[5] >> 4 & 0x03;    //目的:使mouse.press_x对齐
+
+    RC.mouse.x = (uint16_t)RC_Data[6] | (uint16_t)RC_Data[7] << 8;
+    RC.mouse.y = (uint16_t)RC_Data[8] | (uint16_t)RC_Data[9] << 8;
+    RC.mouse.z = (uint16_t)RC_Data[10] | (uint16_t)RC_Data[11] << 8;
+    RC.mouse.press_l = RC_Data[12];
+    RC.mouse.press_r = RC_Data[13];
+
+    RC.key.all = (uint16_t)RC_Data[14] | (uint16_t)RC_Data[15];
+}
+
+//补充:错误消息
+//侧拨轮
---
+++ connectivity/uart/dbus.h
@@ -0,0 +1,59 @@
+//
+// Created by DingYong on 25-8-5.
+//
+
+#ifndef DBUS_H
+#define DBUS_H
+#include <stdint.h>
+
+
+//按键掩码
+#define KEY_MASK_W               ((uint16_t)0x01 << 0)
+#define KEY_MASK_S               ((uint16_t)0x01 << 1)
+#define KEY_MASK_A               ((uint16_t)0x01 << 2)
+#define KEY_MASK_D               ((uint16_t)0x01 << 3)
+#define KEY_MASK_Q               ((uint16_t)0x01 << 4)
+#define KEY_MASK_E               ((uint16_t)0x01 << 5)
+#define KEY_MASK_SHIFT           ((uint16_t)0x01 << 6)
+#define KEY_MASK_CTRL            ((uint16_t)0x01 << 7)
+
+//按键检测宏(检测按键状态)
+//若对应按键被按下,则与对应按键掩码相与后逻辑为1->按下,否则为0->未按下
+#define KEY_PRESSED_W            ((RC.key.all & KEY_MASK_W) != 0)   //&优先级低于!=
+#define KEY_PRESSED_S            ((RC.key.all & KEY_MASK_S) != 0)
+#define KEY_PRESSED_A            ((RC.key.all & KEY_MASK_A) != 0)
+#define KEY_PRESSED_D            ((RC.key.all & KEY_MASK_D) != 0)
+#define KEY_PRESSED_Q            ((RC.key.all & KEY_MASK_Q) != 0)
+#define KEY_PRESSED_E            ((RC.key.all & KEY_MASK_E) != 0)
+#define KEY_PRESSED_SHIFT        ((RC.key.all & KEY_MASK_SHIFT) != 0)
+#define KEY_PRESSED_CTRL         ((RC.key.all & KEY_MASK_CTRL) != 0)
+
+
+typedef struct {
+    int16_t ch0;
+    int16_t ch1;
+    int16_t ch2;
+    int16_t ch3;
+
+    int8_t s1;
+    int8_t s2;
+
+    struct {
+        int16_t x;
+        int16_t y;
+        int16_t z;
+
+        uint8_t press_l;
+        uint8_t press_r;
+    }mouse;
+
+    struct {
+        uint16_t all;
+    }key;
+
+}RC_t;
+
+
+void rcDecode(void);
+
+#endif //DBUS_H
