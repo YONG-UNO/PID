0c8368336e0449e52ea7a31b86293e308cf3b9d7
Ding Yong <2903964879@qq.com>
2025-08-07 16:10:01 +0800

    add bsp_CAN(仅写完接收)

---
+++ connectivity/CAN/bsp_CAN.h
@@ -0,0 +1,33 @@
+//
+// Created by DingYong on 25-8-7.
+//
+
+#ifndef MYCAN_H
+#define MYCAN_H
+
+#include "stm32f4xx.h"
+
+#include "stm32f4xx_hal_can.h"
+
+# define  CAN_3508_M1_ID    0x201
+# define  CAN_3508_M2_ID    0x202
+# define  CAN_3508_M3_ID    0x203
+# define  CAN_3508_M4_ID    0x204
+# define  CAN_6020_M5_ID    0x205
+
+
+typedef struct {
+    uint16_t ecd;        //6020:机械角[0-8191]
+    int16_t  speed_rpm;
+    int16_t  given_current;
+    uint8_t  temperate;
+    int16_t  last_ecd;
+} motor_measure_t;
+
+extern motor_measure_t motor_gimbal[8];
+
+void CAN_Filter_Init(void);
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan);
+void get_motor_measure(motor_measure_t *motor_measure, uint8_t rx_data[]);
+
+#endif //MYCAN_H
