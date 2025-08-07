658faf722acedb953b3901d36070204e9bdd8a3f
Ding Yong <2903964879@qq.com>
2025-08-08 00:43:26 +0800

    add bsp_CAN(接收验证完成)

--- connectivity/CAN/bsp_CAN.c
+++ connectivity/CAN/bsp_CAN.c
@@ -3,14 +3,15 @@
 //
 
 
-#include "myCan.h"
+#include "bsp_CAN.h"
 
 #include "can.h"
 #include "main.h"
 
+CAN_RxHeaderTypeDef rx_header;
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
-    CAN_RxHeaderTypeDef rx_header;  //定义一个can消息头结构体变量,存储收到的can消息头部信息
-                                    //包含消息id(StdId),帧类型(IDE,标准帧或拓展帧),消息类型(RTR,数据帧还是远程帧),数据长度(DLC,0-8字节)
+      //定义一个can消息头结构体变量,存储收到的can消息头部信息
+    //包含消息id(StdId),帧类型(IDE,标准帧或拓展帧),消息类型(RTR,数据帧还是远程帧),数据长度(DLC,0-8字节)
 
     uint8_t rx_data[8];             //存储收到的CAN数据场(CAN最大支持8Byte数据)
 
@@ -19,12 +20,11 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
     switch (rx_header.StdId) {
         case CAN_3508_M1_ID:        //当收到的CAN消息ID(rx_header.StdId)
         case CAN_3508_M2_ID:        //与switch中的任意一个ID匹配时,就会执行{}中的代码
-        case CAN_3508_M3_ID:
-        case CAN_3508_M4_ID:
+        case CAN_2006_M3_ID:
         case CAN_6020_M5_ID: {
 
             uint8_t n = rx_header.StdId - 0x201;  //3508: 0x200+n   6020:0x204+n
-            get_motor_measure(&motor_gimbal[n], rx_data);
+            get_motor_measure(&motor_gimbal[n], rx_header.StdId ,rx_data);
 
             break;
 
@@ -35,18 +35,28 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
     }
 }
 
-void get_motor_measure(motor_measure_t *motor_measure,uint8_t rx_data[]) {
-    motor_measure->last_ecd      =  motor_measure->ecd;
-    motor_measure->ecd           =  ((uint16_t)rx_data[0] << 8  | (uint16_t)rx_data[1]);
-    motor_measure->speed_rpm     =  ((int16_t) rx_data[2] << 8  | (int16_t)rx_data[3]);
-    motor_measure->given_current =  ((int16_t) rx_data[4] << 8  | (int16_t)rx_data[5]);
-    motor_measure->temperate     =  rx_data[6];
+void get_motor_measure(motor_measure_t *motor_measure,uint32_t StdId, uint8_t rx_data[]) {
+    // motor_measure->last_ecd      =  motor_measure->ecd;
+    // motor_measure->ecd           =  ((uint16_t)rx_data[0] << 8  | (uint16_t)rx_data[1]);
+    // motor_measure->speed_rpm     =  ((int16_t) rx_data[2] << 8  | (int16_t)rx_data[3]);
+    // motor_measure->given_current =  ((int16_t) rx_data[4] << 8  | (int16_t)rx_data[5]);
+    // motor_measure->temperate     =  rx_data[6];
+
+    switch (StdId){
+        case CAN_2006_M3_ID:
+            motor_measure->ecd           = ((uint16_t)rx_data[0] << 8  | (uint16_t)rx_data[1]);
+            motor_measure->speed_rpm     = ((int16_t) rx_data[2] << 8  | (int16_t)rx_data[3]);
+            motor_measure->torque        = ((int16_t) rx_data[4] << 8  | (int16_t)rx_data[5]);
+    }
+
 }
 
 
 void CAN_Filter_Init(void)
 {
-    CAN_FilterTypeDef can1_filter_st,can2_filter_st;
+    CAN_FilterTypeDef can1_filter_st;
+    // CAN_FilterTypeDef can2_filter_st;
+
 
     can1_filter_st.FilterIdHigh = 0x0000;
     can1_filter_st.FilterIdLow = 0x0000;
@@ -59,16 +69,16 @@ void CAN_Filter_Init(void)
     can1_filter_st.FilterBank = 0;
     can1_filter_st.SlaveStartFilterBank = 14;
 
-    can2_filter_st.FilterIdHigh = 0x0000;
-    can2_filter_st.FilterIdLow = 0x0000;
-    can2_filter_st.FilterMaskIdHigh = 0x0000;
-    can2_filter_st.FilterMaskIdLow = 0x0000;
-    can2_filter_st.FilterFIFOAssignment = CAN_RX_FIFO1;
-    can2_filter_st.FilterActivation = ENABLE;
-    can2_filter_st.FilterMode = CAN_FILTERMODE_IDMASK;
-    can2_filter_st.FilterScale = CAN_FILTERSCALE_32BIT;
-    can2_filter_st.FilterBank = 14;
-    can2_filter_st.SlaveStartFilterBank = 14;
+    // can2_filter_st.FilterIdHigh = 0x0000;
+    // can2_filter_st.FilterIdLow = 0x0000;
+    // can2_filter_st.FilterMaskIdHigh = 0x0000;
+    // can2_filter_st.FilterMaskIdLow = 0x0000;
+    // can2_filter_st.FilterFIFOAssignment = CAN_RX_FIFO1;
+    // can2_filter_st.FilterActivation = ENABLE;
+    // can2_filter_st.FilterMode = CAN_FILTERMODE_IDMASK;
+    // can2_filter_st.FilterScale = CAN_FILTERSCALE_32BIT;
+    // can2_filter_st.FilterBank = 14;
+    // can2_filter_st.SlaveStartFilterBank = 14;
     //使能CAN通道
     if (HAL_CAN_ConfigFilter(&hcan1, &can1_filter_st) != HAL_OK)// 配置 CAN1 过滤器
     {
@@ -82,16 +92,17 @@ void CAN_Filter_Init(void)
     {
         Error_Handler();
     }
-    HAL_Delay(10);
-    if (HAL_CAN_ConfigFilter(&hcan2, &can2_filter_st) != HAL_OK)    // 配置 CAN2 过滤器
-    {
-        Error_Handler();
-    }
-    if (HAL_CAN_Start(&hcan2) != HAL_OK)// 启动 CAN2
-    {
-        Error_Handler();
-    }
-    if (HAL_CAN_ActivateNotification(&hcan2, CAN_IT_RX_FIFO1_MSG_PENDING) != HAL_OK)// 使能 CAN2 接收 FIFO1 消息中断
-    {
-        Error_Handler();
-    }}
+    // HAL_Delay(10);
+    // if (HAL_CAN_ConfigFilter(&hcan2, &can2_filter_st) != HAL_OK)    // 配置 CAN2 过滤器
+    // {
+    //     Error_Handler();
+    // }
+    // if (HAL_CAN_Start(&hcan2) != HAL_OK)// 启动 CAN2
+    // {
+    //     Error_Handler();
+    // }
+    // if (HAL_CAN_ActivateNotification(&hcan2, CAN_IT_RX_FIFO1_MSG_PENDING) != HAL_OK)// 使能 CAN2 接收 FIFO1 消息中断
+    // {
+    //     Error_Handler();
+    // }
+}
--- connectivity/CAN/bsp_CAN.h
+++ connectivity/CAN/bsp_CAN.h
@@ -5,15 +5,14 @@
 #ifndef MYCAN_H
 #define MYCAN_H
 
-#include "stm32f4xx.h"
+#include "stm32f4xx_hal.h"
 
 #include "stm32f4xx_hal_can.h"
 
 # define  CAN_3508_M1_ID    0x201
 # define  CAN_3508_M2_ID    0x202
-# define  CAN_3508_M3_ID    0x203
-# define  CAN_3508_M4_ID    0x204
-# define  CAN_6020_M5_ID    0x205
+# define  CAN_2006_M3_ID    0x203
+# define  CAN_6020_M5_ID    0x208
 
 
 typedef struct {
@@ -22,12 +21,13 @@ typedef struct {
     int16_t  given_current;
     uint8_t  temperate;
     int16_t  last_ecd;
+    uint16_t torque;
 } motor_measure_t;
 
 extern motor_measure_t motor_gimbal[8];
 
 void CAN_Filter_Init(void);
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan);
-void get_motor_measure(motor_measure_t *motor_measure, uint8_t rx_data[]);
+void get_motor_measure(motor_measure_t *motor_measure,uint32_t StdId, uint8_t rx_data[]);
 
 #endif //MYCAN_H
